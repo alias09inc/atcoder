b5af71d192fe144fa93ddfb74c4cee4dbf3e38bb
alias09inc <kenshi06182001@gmail.com>
2024-10-05 21:02:11 +0900

    j

---
+++ ABC/abc373/a/main.cpp
@@ -0,0 +1,16 @@
+// #include <bits/stdc++.h>
+
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+#include <string>
+#include <iostream>
+int main() {
+    string st;
+    rep(0,12){
+        cin >> st;
+        if len(st) == i;
+    }
+}
